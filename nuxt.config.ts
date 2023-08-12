a648c567b014b5458a849b2abe4acd8f36aa8bbf
Rado <r.sabo@outlook.com>
2023-08-12 14:30:17 +0200

    test authjs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ["@hebilicious/authjs-nuxt"],
+  runtimeConfig: {
+    authJs: {
+      secret: process.env.NUXT_NEXTAUTH_SECRET // You can generate one with `openssl rand -base64 32`
+    },
+    github: {
+      clientId: process.env.NUXT_GITHUB_CLIENT_ID,
+      clientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET
+    },
+    public: {
+      authJs: {
+        baseUrl: process.env.NUXT_NEXTAUTH_URL, // The base URL is used for the Origin Check in prod only
+        verifyClientOnEveryRequest: true // whether to hit the /auth/session endpoint on every client request
+      }
+    }
+  }
 })

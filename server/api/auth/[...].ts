adc090e75c650eafc24af603f96c74721ef6790a
Rado <r.sabo@outlook.com>
2023-08-13 09:44:28 +0200

    add prisma client

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,12 +1,15 @@
 import type { AuthConfig } from "@auth/core/types"
 import { NuxtAuthHandler } from "#auth"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
 
 // The #auth virtual import comes from this module. You can use it on the client
 // and server side, however not every export is universal. For example do not
 // use sign-in and sign-out on the server side.
 
 const runtimeConfig = useRuntimeConfig()
+
+const prisma = new PrismaClient()
 
 // Refer to Auth.js docs for more details
 export const authOptions: AuthConfig = {
